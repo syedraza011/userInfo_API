825f6f1ee169837b1e2e8fabdae0b32bf2b4a9fe
Syed Raza <syed.raza011@gmail.com>
2023-06-19 14:57:21 -0400

    fix the user view , update few fonts and spaces so data looks better

--- userInfo/ContentView.swift
+++ userInfo/ContentView.swift
@@ -1,49 +1,47 @@
+////
+////  ContentView.swift
+////  userInfo
+////
+////  Created by Syed Raza on 6/19/23.
+////
 //
-//  ContentView.swift
-//  userInfo
 //
-//  Created by Syed Raza on 6/19/23.
 //
-
-
-
+//
 import SwiftUI
 
 struct ContentView: View {
     @StateObject var viewModel = UserViewModel()
-    
+
     var body: some View {
         NavigationView {
             VStack {
                 List(viewModel.users) { user in
                     NavigationLink(destination: UserDetailsView(user: user)) {
-                        HStack {
-                            Text("\(user.id)")
-                                .font(.subheadline)
-                                .foregroundColor(.white)
-                            
-                            Text(user.name)
-                            
-                                .foregroundColor(.primary)
-                        }
-                        .padding(8)
-                        
-                        
-                        
-                        VStack(alignment: .leading) {
-                            Text(user.email)
-                            
-                            
-                            Text(user.phone)
-                            
-                            
+                        VStack(alignment: .leading, spacing: 4) { // Add VStack for ID, name, email, and phone
+                            HStack {
+                                Text("\(user.id)")
+                                    .font(.subheadline)
+                                    .foregroundColor(.black )
+                                Text(user.name)
+                                    .foregroundColor(.primary)
+                            }
+                            HStack {
+                                Spacer().frame(width: 15)
+                                Text(user.email)
+                                Spacer()
+                            }
+                            HStack {
+                                Spacer().frame(width: 15)
+                                Text(user.phone)
+                                Spacer()
+                            }
                         }
                         .padding(10)
-                        
+                        .background(Color.blue.opacity(0.2))
+                        .foregroundColor(.black)
+                        .cornerRadius(20)
                     }
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(15)
                 }
                 .listStyle(.plain)
             }
@@ -60,7 +58,7 @@ struct ContentView: View {
 
 struct UserDetailsView: View {
     let user: User
-    
+
     var body: some View {
         VStack {
             Text(user.name)
@@ -86,7 +84,84 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-
-
-
-
+//import SwiftUI
+//
+//struct ContentView: View {
+//    @StateObject var viewModel = UserViewModel()
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                List(viewModel.users) { user in
+//                    NavigationLink(destination: UserDetailsView(user: user)) {
+//                        HStack {
+//                            Text("\(user.id)")
+//                                .font(.subheadline)
+//                                .foregroundColor(.white)
+//                            Text(user.name)
+//
+//                                .foregroundColor(.primary)
+//                        }
+//
+//
+//
+//                        VStack(alignment: .leading) {
+//                            Text(user.email)
+//
+//
+//                            Text(user.phone)
+//
+//
+//                        }
+//                        .padding(10)
+//
+//                    }
+//                    .background(Color.blue)
+//                    .foregroundColor(.white)
+//                    .cornerRadius(15)
+//                }
+//                .listStyle(.plain)
+//            }
+//            .navigationBarTitle("PhoneBook")
+//            .onAppear {
+//                viewModel.getUsers()
+//            }
+//            .padding()
+//        }
+//        .background(Color.green)
+//        .accentColor(.blue)
+//    }
+//}
+//
+//struct UserDetailsView: View {
+//    let user: User
+//
+//    var body: some View {
+//        VStack {
+//            Text(user.name)
+//                .font(.title)
+//            Text("ID: \(user.id)")
+//                .font(.subheadline)
+//                .foregroundColor(.secondary)
+//            Text("Email: \(user.email)")
+//                .font(.subheadline)
+//                .foregroundColor(.secondary)
+//            Text("Phone: \(user.phone)")
+//                .font(.subheadline)
+//                .foregroundColor(.secondary)
+//        }
+//        .padding()
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+//
+//
+//
+//
+//
+//
