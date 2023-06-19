69313cb419d28ffe5c6befb2eae7483eab88d5be
Syed Raza <syed.raza011@gmail.com>
2023-06-19 12:16:45 -0400

    Initial Commit

---
+++ userInfo/userInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  userInfoApp.swift
+//  userInfo
+//
+//  Created by Syed Raza on 6/19/23.
+//
+
+import SwiftUI
+
+@main
+struct userInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
