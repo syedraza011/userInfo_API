c0cc0d41af115101cd6155e8bc76835848b86145
Syed Raza <syed.raza011@gmail.com>
2023-06-19 16:01:28 -0400

    final

--- userInfo/ContentView.swift
+++ userInfo/ContentView.swift
@@ -58,29 +58,58 @@ struct UserDetailsView: View {
 
     var body: some View {
         VStack(spacing: 10) {
-            
-            Text("\(user.id)")
-            Text(user.name)
-            Text(user.username)
-            Text(user.email)
-            Text(user.address.street)
-            Text(user.address.suite)
-            Text(user.address.city)
-            VStack(spacing: 10) {
-                Text(user.address.zipcode)
-                Text(user.address.geo.lat)
-                Text(user.address.geo.lng)
-                Text(user.phone)
-                Text(user.website)
-                Text(user.company.name)
-                Text(user.company.catchPhrase)
-                Text(user.company.bs)
-            }
-    }
-
-        .padding()
-    }
-}
+                  Text("\(user.id)")
+                      .font(.headline)
+                      .foregroundColor(.white)
+                      .padding()
+                      .background(Color.blue)
+                      .cornerRadius(10)
+                  
+                  Text(user.name)
+                      .font(.title)
+                      .fontWeight(.bold)
+                      .foregroundColor(.primary)
+                  
+                  Text(user.username)
+                      .font(.subheadline)
+                      .foregroundColor(.secondary)
+                  
+                  Text(user.email)
+                      .font(.subheadline)
+                      .foregroundColor(.secondary)
+                  
+                  Text(user.address.street)
+                      .font(.subheadline)
+                      .foregroundColor(.secondary)
+                  
+                  Text(user.address.suite)
+                      .font(.subheadline)
+                      .foregroundColor(.secondary)
+                  
+                  Text(user.address.city)
+                      .font(.subheadline)
+                      .foregroundColor(.secondary)
+                  
+                  VStack(spacing: 10) {
+                      Text(user.address.zipcode)
+                      Text(user.address.geo.lat)
+                      Text(user.address.geo.lng)
+                      Text(user.phone)
+                      Text(user.website)
+                      Text(user.company.name)
+                      Text(user.company.catchPhrase)
+                      Text(user.company.bs)
+                  }
+                  .padding()
+                  .background(Color.gray)
+                  .cornerRadius(20)
+              }
+              .padding()
+              .background(Color.white)
+              .cornerRadius(10)
+              .shadow(color: Color.black.opacity(0.9), radius: 5, x: 0, y: 2)
+          }
+      }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- userInfo/New Group/SingleUserCell.swift
+++ userInfo/New Group/SingleUserCell.swift
@@ -24,33 +24,98 @@ import SwiftUI
 //        }
 //    }
 //}
+//struct SingleUserCell: View {
+//    let user: User
+//
+//    var body: some View {
+//
+//            VStack(spacing: 10) {
+//
+//                Text("\(user.id)")
+//                Text(user.name)
+//                Text(user.username)
+//                Text(user.email)
+//                Text(user.address.street)
+//                Text(user.address.suite)
+//                Text(user.address.city)
+//                VStack(spacing: 10) {
+//                    Text(user.address.zipcode)
+//                    Text(user.address.geo.lat)
+//                    Text(user.address.geo.lng)
+//                    Text(user.phone)
+//                    Text(user.website)
+//                    Text(user.company.name)
+//                    Text(user.company.catchPhrase)
+//                    Text(user.company.bs)
+//                }
+//        }
+//    }
+//}
 struct SingleUserCell: View {
     let user: User
     
     var body: some View {
-   
+        VStack(spacing: 10) {
+            Text("\(user.id)")
+                .font(.headline)
+                .foregroundColor(.blue)
+                .fontWeight(.bold)
+            Text(user.name)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.primary)
+            Text(user.username)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            Text(user.email)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            Text(user.address.street)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            Text(user.address.suite)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            Text(user.address.city)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+            
             VStack(spacing: 10) {
-                
-                Text("\(user.id)")
-                Text(user.name)
-                Text(user.username)
-                Text(user.email)
-                Text(user.address.street)
-                Text(user.address.suite)
-                Text(user.address.city)
-                VStack(spacing: 10) {
-                    Text(user.address.zipcode)
-                    Text(user.address.geo.lat)
-                    Text(user.address.geo.lng)
-                    Text(user.phone)
-                    Text(user.website)
-                    Text(user.company.name)
-                    Text(user.company.catchPhrase)
-                    Text(user.company.bs)
-                }
+                Text(user.address.zipcode)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(user.address.geo.lat)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(user.address.geo.lng)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(user.phone)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(user.website)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                Text(user.company.name)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .italic()
+                Text(user.company.catchPhrase)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .italic()
+                Text(user.company.bs)
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .italic()
+            }
         }
+        .padding()
+        .background(Color.yellow)
+        .cornerRadius(10)
     }
 }
+
 
 struct SingleUserCell_Previews: PreviewProvider {
     static var previews: some View {
